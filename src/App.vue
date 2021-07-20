a9840a8ab6a0677c5a9e833451b85fdee5ec2aca
Pavel <hawkrammfan@gmail.com>
2021-07-20 17:25:15 +0300

    Удаление поста

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <post-form 
       @create="createPost" />
     <post-list 
-      :posts="posts" />
+      :posts="posts" 
+      @remove = "removePost"
+    />
   </div>
 </template>
 
@@ -29,6 +31,9 @@ export default {
     createPost(post) {
       this.posts.push(post);
     },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    }
   },
 };
 </script>
