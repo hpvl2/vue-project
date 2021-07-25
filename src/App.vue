85c79fa18871b5f646d4f0dd46a0e5d6ffc1b959
Pavel <hawkrammfan@gmail.com>
2021-07-25 12:52:02 +0300

    add component MySelect

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
 	<div class="app">
 		<h1>Страница с постами</h1>
-		<my-button @click="fetchPosts">
-			Получить посты
-		</my-button>
-		<my-button @click="showDialog" style="margin: 15px 0;">
-			Создать пост
-		</my-button>
+		<div class="app__btns">
+			<my-button @click="showDialog">
+				Создать пост
+			</my-button>
+			<my-select v-model="selectedSort" :options="sortOptions" />
+		</div>
 		<my-dialog v-model:show="dialogVisible">
 			<post-form @create="createPost" />
 		</my-dialog>
-		<post-list :posts="posts" @remove="removePost" v-if="!isPostsLoading" />
+		<post-list
+			:posts="sortedPosts"
+			@remove="removePost"
+			v-if="!isPostsLoading"
+		/>
 		<div v-else>Идет загрузка...</div>
 	</div>
 </template>
@@ -29,6 +33,12 @@ export default {
 			posts: [],
 			dialogVisible: false,
 			isPostsLoading: false,
+			selectedSort: '',
+			sortOptions: [
+				{ value: 'title', name: 'По названию' },
+				{ value: 'body', name: 'По содержимому' },
+				{ value: 'id', name: 'По id' },
+			],
 		};
 	},
 	methods: {
@@ -45,10 +55,10 @@ export default {
 		async fetchPosts() {
 			try {
 				this.isPostsLoading = true;
-					const response = await axios.get(
-						'https://jsonplaceholder.typicode.com/posts?_limit=10'
-					);
-					this.posts = response.data;
+				const response = await axios.get(
+					'https://jsonplaceholder.typicode.com/posts?_limit=10'
+				);
+				this.posts = response.data;
 			} catch (e) {
 				alert('Ошибка');
 			} finally {
@@ -58,6 +68,18 @@ export default {
 	},
 	mounted() {
 		this.fetchPosts();
+	},
+	watch: {
+		selectedSort(newValue) {
+			console.log(newValue);
+		},
+	},
+	computed: {
+		sortedPosts() {
+			return [...this.posts].sort((post1, post2) =>
+				post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+			);
+		},
 	},
 };
 </script>
@@ -71,5 +93,11 @@ export default {
 
 .app {
 	padding: 10px;
+}
+
+.app__btns {
+	margin: 15px 0;
+	display: flex;
+	justify-content: space-between;
 }
 </style>
